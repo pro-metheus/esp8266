f0ef0fe46efec4b3f5faa6f9d1936a3b52fa6e3a
pro-metheus <ajayajith19@gmail.com>
2017-06-12 10:48:39 +0530

    Add files via upload
    UltraSonic library for NodeMCU running with micropython

---
+++ UltraSonic.py
@@ -0,0 +1,37 @@
+from machine import Pin
+import time
+
+class  IHandler():   #provides interrupt handlers
+    @staticmethod
+    def rise_handler(p):  #rising edge handler
+        start=time.time()
+        print("started")
+
+    @staticmethod
+    def fall_handler(p):
+        stop=time.time()
+        print("stopped")
+    
+
+
+
+
+class UltraSonic():
+    def __init__(self,trig,echo):
+        self.tp=Pin(trig,Pin.OUT)    #trigger pin set to output
+        self.ep=Pin(trig,Pin.IN)     #echo pin set to input
+        self.start=0
+        self.stop=0s
+
+    
+
+    def pulsein(self):
+        self.tp.value(0)
+        time.sleep(.002)
+        self.tp.value(1)
+        time.sleep(.01)
+        self.tp.value(0)
+        self.ep.irq(trigger=Pin.IRQ_RISING,handler=IHandler.rise_handler)
+        self.ep.irq(trigger=Pin.IRQ_FALLING,handler=IHandler.fall_handler)
+        return self.start-self.stop
+    
